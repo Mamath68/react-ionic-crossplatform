521ca42f3bd4f77ffdf5bd0c52ce5742fcee3f8b
Mamath68 <mathieu.stamm@gmail.Com>
2023-11-26 10:46:26 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'react.ionic.crossplatform',
+  appName: 'react-ionic-crossplatform',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
